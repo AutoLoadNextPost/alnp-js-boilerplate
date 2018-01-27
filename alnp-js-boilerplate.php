176465274149da89b8eac5f4734d3fc7e248f1fe
Sébastien Dumont <mailme@sebastiendumont.com>
2018-01-27 19:10:32 +0100

    Added required ANLP check.

--- alnp-js-boilerplate.php
+++ alnp-js-boilerplate.php
@@ -1,17 +1,19 @@
 <?php
 /*
- * Plugin Name: Auto Load Next Post JS Boilerplate
+ * Plugin Name: Auto Load Next Post: JS Boilerplate
  * Plugin URI:  https://github.com/AutoLoadNextPost/alnp-js-boilerplate
  * Version:     1.0.0
- * Description: Boilerplate for writing plugins for Auto Load Next Post JavaScript
- * Author:      Sébastien Dumont
- * Author URI:  https://sebastiendumont.com
+ * Description: Boilerplate for writing plugins for Auto Load Next Post JavaScript.
+ * Author: Auto Load Next Post
+ * Author URI: https://autoloadnextpost.com
+ * Developer: Sébastien Dumont
+ * Developer URI: https://sebastiendumont.com
  *
  * Text Domain: alnp-js-boilerplate
  * Domain Path: /languages/
  *
  * Requires at least: 4.5
- * Tested up to: 4.9.1
+ * Tested up to: 4.9.2
  *
  * Copyright: © 2018 Sébastien Dumont
  * License: GNU General Public License v3.0
@@ -29,6 +31,14 @@ if ( ! class_exists( 'ALNP_JS_Boilerplate' ) ) {
 		 * @since  1.0.0
 		 */
 		public static $version = '1.0.0';
+
+		/**
+		 * Required Auto Load Next Post Version
+		 *
+		 * @access public
+		 * @since  1.0.0
+		 */
+		public $required_alnp = '1.4.8';
 
 		/**
 		 * @var ALNP_JS_Boilerplate - the single instance of the class.
@@ -64,7 +74,7 @@ if ( ! class_exists( 'ALNP_JS_Boilerplate' ) ) {
 		 * @since  1.0.0
 		 */
 		public function __clone() {
-			_doing_it_wrong( __FUNCTION__, __( 'Foul!', 'alnp-js-boilerplate' ), '1.0.0' );
+			_doing_it_wrong( __FUNCTION__, __( 'Foul!', 'alnp-js-boilerplate' ), self::$version );
 		}
 
 		/**
@@ -74,7 +84,7 @@ if ( ! class_exists( 'ALNP_JS_Boilerplate' ) ) {
 		 * @since  1.0.0
 		 */
 		public function __wakeup() {
-			_doing_it_wrong( __FUNCTION__, __( 'Foul!', 'alnp-js-boilerplate' ), '1.0.0' );
+			_doing_it_wrong( __FUNCTION__, __( 'Foul!', 'alnp-js-boilerplate' ), self::$version );
 		}
 
 		/**
@@ -94,10 +104,36 @@ if ( ! class_exists( 'ALNP_JS_Boilerplate' ) ) {
 		 * @since  1.0.0
 		 */
 		private function init_hooks() {
-			add_action( 'init', array( $this, 'init_plugin' ), 0 );
+			add_action( 'plugin_loaded', array( $this, 'check_alnp_installed' ) );
+			add_action( 'init', array( $this, 'load_text_domain' ), 0 );
 
 			add_action( 'wp_enqueue_scripts', array( $this, 'alnp_enqueue_scripts' ) );
-		} // END load_plugin()
+		} // END init_hooks()
+
+		/**
+		 * Checks if Auto Load Next Post is installed.
+		 *
+		 * @access public
+		 * @since  1.0.0
+		 * @return bool
+		 */
+		public function check_alnp_installed() {
+			if ( ! defined( 'AUTO_LOAD_NEXT_POST_VERSION' ) || version_compare( AUTO_LOAD_NEXT_POST_VERSION, $this->required_alnp, '<' ) ) {
+				add_action( 'admin_notices', array( $this, 'alnp_not_installed' ) );
+				return false;
+			}
+		} // END check_alnp_installed()
+
+		/**
+		 * Auto Load Next Post is Not Installed Notice.
+		 *
+		 * @access public
+		 * @since  1.0.0
+		 * @return void
+		 */
+		public function alnp_not_installed() {
+			echo '<div class="error"><p>' . sprintf( __( 'Auto Load Next Post: JS Boilerplate requires $1%s v$2%s or higher to be installed.', 'alnp-js-boilerplate' ), '<a href="https://autoloadnextpost.com/" target="_blank">Auto Load Next Post</a>', $this->required_alnp ) . '</p></div>';
+		} // END alnp_not_installed()
 
 		/**
 		 * Get the Plugin URL.
@@ -112,16 +148,15 @@ if ( ! class_exists( 'ALNP_JS_Boilerplate' ) ) {
 		} // END plugin_url()
 
 		/**
-		 * Initialize the plugin if ready.
+		 * Load the plugin text domain once the plugin has initialized.
 		 *
 		 * @access public
 		 * @since  1.0.0
 		 * @return void
 		 */
-		public function init_plugin() {
-			// Load text domain.
+		public function load_text_domain() {
 			load_plugin_textdomain( 'alnp-js-boilerplate', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-		} // END init_plugin()
+		} // END load_text_domain()
 
 		/**
 		 * Load JS only on the front end for a single post.
