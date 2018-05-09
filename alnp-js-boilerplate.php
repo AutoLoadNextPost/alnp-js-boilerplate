633a2bfc2c676e5670f70cb496f52c1d3bd7c011
Sébastien Dumont <mailme@sebastiendumont.com>
2018-05-09 02:50:26 +0200

    Main plugin file improved.

--- alnp-js-boilerplate.php
+++ alnp-js-boilerplate.php
@@ -1,27 +1,38 @@
 <?php
 /*
  * Plugin Name: Auto Load Next Post: JS Boilerplate
- * Plugin URI: https://github.com/AutoLoadNextPost/alnp-js-boilerplate
- * Version: 1.0.0
+ * Plugin URI:  https://github.com/AutoLoadNextPost/alnp-js-boilerplate
  * Description: Boilerplate for writing plugins for Auto Load Next Post JavaScript.
  * Author: Auto Load Next Post
  * Author URI: https://autoloadnextpost.com
+ * Version: 1.0.0
  * Developer: Sébastien Dumont
  * Developer URI: https://sebastiendumont.com
- *
  * Text Domain: alnp-js-boilerplate
  * Domain Path: /languages/
  *
- * Requires at least: 4.5
- * Tested up to: 4.9.2
- *
  * Copyright: © 2018 Sébastien Dumont
+ *
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package   Auto Load Next Post: JS Boilerplate
+ * @author    Auto Load Next Post
+ * @copyright Copyright © 2018, Auto Load Next Post
+ * @license   GNU General Public License v3.0 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
 if ( ! class_exists( 'ALNP_JS_Boilerplate' ) ) {
 	class ALNP_JS_Boilerplate {
+
+		/**
+		 * @var ALNP_JS_Boilerplate - the single instance of the class.
+		 *
+		 * @access protected
+		 * @static
+		 * @since 1.0.0
+		 */
+		protected static $_instance = null;
 
 		/**
 		 * Plugin Version
@@ -41,15 +52,6 @@ if ( ! class_exists( 'ALNP_JS_Boilerplate' ) ) {
 		public $required_alnp = '1.4.8';
 
 		/**
-		 * @var ALNP_JS_Boilerplate - the single instance of the class.
-		 *
-		 * @access protected
-		 * @static
-		 * @since 1.0.0
-		 */
-		protected static $_instance = null;
-
-		/**
 		 * Main ALNP_JS_Boilerplate Instance.
 		 *
 		 * Ensures only one instance of ALNP_JS_Boilerplate is loaded or can be loaded.
@@ -65,7 +67,7 @@ if ( ! class_exists( 'ALNP_JS_Boilerplate' ) ) {
 				self::$_instance = new self();
 			}
 			return self::$_instance;
-		}
+		} // END instance()
 
 		/**
 		 * Cloning is forbidden.
@@ -74,8 +76,8 @@ if ( ! class_exists( 'ALNP_JS_Boilerplate' ) ) {
 		 * @since  1.0.0
 		 */
 		public function __clone() {
-			_doing_it_wrong( __FUNCTION__, __( 'Foul!', 'alnp-js-boilerplate' ), self::$version );
-		}
+			_doing_it_wrong( __FUNCTION__, __( 'Cloning this object is forbidden.', 'alnp-js-boilerplate' ), self::$version );
+		} // END __clone()
 
 		/**
 		 * Unserializing instances of this class is forbidden.
@@ -84,18 +86,19 @@ if ( ! class_exists( 'ALNP_JS_Boilerplate' ) ) {
 		 * @since  1.0.0
 		 */
 		public function __wakeup() {
-			_doing_it_wrong( __FUNCTION__, __( 'Foul!', 'alnp-js-boilerplate' ), self::$version );
-		}
+			_doing_it_wrong( __FUNCTION__, __( 'Unserializing instances of this class is forbidden.', 'alnp-js-boilerplate' ), self::$version );
+		} // END __wakeup()
 
 		/**
-		 * Load the plugin.
+		 * ALNP_JS_Boilerplate Constructor.
 		 *
 		 * @access public
 		 * @since  1.0.0
+		 * @return ALNP_JS_Boilerplate
 		 */
 		public function __construct() {
 			$this->init_hooks();
-		}
+		} // END __construct()
 
 		/**
 		 * Initialize hooks.
@@ -104,20 +107,20 @@ if ( ! class_exists( 'ALNP_JS_Boilerplate' ) ) {
 		 * @since  1.0.0
 		 */
 		private function init_hooks() {
-			add_action( 'plugin_loaded', array( $this, 'check_alnp_installed' ) );
-			add_action( 'init', array( $this, 'load_text_domain' ), 0 );
+			add_action( 'auto_load_next_post_loaded', array( $this, 'check_required_version' ) );
+			add_action( 'init', array( $this, 'load_plugin_textdomain' ), 0 );
 
 			add_action( 'wp_enqueue_scripts', array( $this, 'alnp_enqueue_scripts' ) );
 		} // END init_hooks()
 
 		/**
-		 * Checks if Auto Load Next Post is installed.
+		 * Checks if the required Auto Load Next Post is installed.
 		 *
 		 * @access public
 		 * @since  1.0.0
 		 * @return bool
 		 */
-		public function check_alnp_installed() {
+		public function check_required_version() {
 			if ( ! defined( 'AUTO_LOAD_NEXT_POST_VERSION' ) || version_compare( AUTO_LOAD_NEXT_POST_VERSION, $this->required_alnp, '<' ) ) {
 				add_action( 'admin_notices', array( $this, 'alnp_not_installed' ) );
 				return false;
@@ -125,7 +128,7 @@ if ( ! class_exists( 'ALNP_JS_Boilerplate' ) ) {
 		} // END check_alnp_installed()
 
 		/**
-		 * Auto Load Next Post is Not Installed Notice.
+		 * Required version of Auto Load Next Post is Not Installed Notice.
 		 *
 		 * @access public
 		 * @since  1.0.0
@@ -134,6 +137,10 @@ if ( ! class_exists( 'ALNP_JS_Boilerplate' ) ) {
 		public function alnp_not_installed() {
 			echo '<div class="error"><p>' . sprintf( __( 'Auto Load Next Post: JS Boilerplate requires $1%s v$2%s or higher to be installed.', 'alnp-js-boilerplate' ), '<a href="https://autoloadnextpost.com/" target="_blank">Auto Load Next Post</a>', $this->required_alnp ) . '</p></div>';
 		} // END alnp_not_installed()
+
+		/*-----------------------------------------------------------------------------------*/
+		/*  Helper Functions                                                                 */
+		/*-----------------------------------------------------------------------------------*/
 
 		/**
 		 * Get the Plugin URL.
@@ -147,16 +154,23 @@ if ( ! class_exists( 'ALNP_JS_Boilerplate' ) ) {
 			return plugins_url( basename( plugin_dir_path(__FILE__) ), basename( __FILE__ ) );
 		} // END plugin_url()
 
+		/*-----------------------------------------------------------------------------------*/
+		/*  Localization                                                                     */
+		/*-----------------------------------------------------------------------------------*/
+
 		/**
-		 * Load the plugin text domain once the plugin has initialized.
+		 * Make the plugin translation ready.
+		 *
+		 * Translations should be added in the WordPress language directory:
+		 *  - WP_LANG_DIR/plugins/alnp-js-boilerplate-LOCALE.mo
 		 *
 		 * @access public
 		 * @since  1.0.0
 		 * @return void
 		 */
-		public function load_text_domain() {
+		public function load_plugin_textdomain() {
 			load_plugin_textdomain( 'alnp-js-boilerplate', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-		} // END load_text_domain()
+		} // END load_plugin_textdomain()
 
 		/**
 		 * Load JS only on the front end for a single post.
@@ -167,7 +181,7 @@ if ( ! class_exists( 'ALNP_JS_Boilerplate' ) ) {
 		 */
 		public function alnp_enqueue_scripts() {
 			if ( is_singular() && get_post_type() == 'post' ) {
-				wp_register_script( 'alnp-js-boilerplate', $this->plugin_url() . '/assets/js/alnp-js-boilerplate.js', array( 'jquery' ), self::$version );
+				wp_register_script( 'alnp-js-boilerplate', $this->plugin_url() . '/assets/js/alnp-js-boilerplate.js', array( 'jquery' ), self::$version, true );
 				wp_enqueue_script( 'alnp-js-boilerplate' );
 			}
 		} // END alnp_enqueue_scripts()
